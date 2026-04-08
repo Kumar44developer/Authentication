f326ce20dd92232077a3abcec263785dac738cab
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-04-08 17:38:25 +0530

    inserted new user into database and render secrets page

--- index.js
+++ index.js
@@ -45,8 +45,13 @@ app.post("/register", async (req, res) => {
     if (checkResult.rows.length > 0) {
       res.send("Email already exists. Try logging in.");
     } else {
-
-
+      const result = await db.query(
+        "INSERT INTO users (email, password) VALUES ($1, $2)",
+        [email, password]
+      );
+      console.log(result);
+      res.render("secrets.ejs");
+    }
 
 
 
